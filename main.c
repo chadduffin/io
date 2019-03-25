1d92d3c820a6aeb77428d00de85d2cb99e9a4c47
Chad Duffin <chadduffin@dungeonsofdoom.local>
2019-03-24 19:34:31 -0700

    add colors

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,7 +10,9 @@
 #include <windows.h>
 #endif
 
-struct {
+#define COLOR(r,g,b) (16 + 36 * r + 6 * g + b)
+
+struct TERM {
   int rows_;
   int cols_;
 #if defined(__APPLE__) || defined(__linux__)
@@ -20,7 +23,7 @@ struct {
 #endif
 } term;
 
-void window_dimensions(int) {
+void window_dimensions(int i) {
   term.rows_ = -1;
   term.cols_ = -1;
 
@@ -46,18 +49,30 @@ char input(void) {
 #if defined(__APPLE__) || defined(__linux__)
   char input = 0;
 
-  read(1, &input, 1);
+  if (read(1, &input, 1) == -1) {
+    return -1;
+  }
 #elif defined(_WIN32)
   DWORD count = 0;
   TCHAR input = 0;
 
   ReadConsole(term.handle_, &input, 1, &count, NULL);
+
+  if (count == 0) {
+    return -1;
+  }
 #endif
 
   return input;
 }
 
 void initialize(void) {
+  setvbuf(stdout, NULL, _IONBF, 0);
+
+  printf("\x1b[?1049h");
+  printf("\x1b[2J");
+  printf("\x1b[?25l");
+
 #if defined(__APPLE__) || defined(__linux__)
   tcgetattr(1, &term.termios_);
 
@@ -71,9 +86,15 @@ void initialize(void) {
   SetConsoleMode(term.handle_,
                  term.mode_ & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
 #endif
+
+  window_dimensions(-1);
 }
 
 void cleanup(void) {
+  printf("\x1b[2J");
+  printf("\x1b[?1049l");
+  printf("\x1b[?25h");
+
 #if defined(__APPLE__) || defined(__linux__)
   tcsetattr(1, TCSANOW, &term.termios_);
 #elif defined(_WIN32)
@@ -81,20 +102,32 @@ void cleanup(void) {
 #endif
 }
 
-void cleanup_and_die(int) {
+void cleanup_and_die(int i) {
   cleanup();
   exit(1);
 }
 
+void draw(char c, int x, int y, int fg, int bg) {
+  printf("\x1b[%i;%iH", y, x);
+  printf("\x1b[38;5;%im", fg);
+  printf("\x1b[48;5;%im", bg);
+  printf("%c", c);
+}
+
 int main(int argc, char** argv) {
+  char c = -1;
+
   atexit(cleanup);
   signal(SIGINT, cleanup_and_die);
   signal(SIGTERM, cleanup_and_die);
+
+#if defined(__APPLE__) || defined(__linux__)
   signal(SIGWINCH, window_dimensions);
+#endif
 
   initialize();
 
-  while (input() != 'q');
+  while ((c = input()) != 'q');
 
   cleanup();
 
